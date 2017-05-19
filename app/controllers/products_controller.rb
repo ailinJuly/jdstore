031eadacc75edd5ac9bac508a11889417bc61ed5
jy <ailinjulygao@gmail.com>
2017-05-20 06:10:11 +0800

    cannot add same item incart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,13 @@ class ProductsController < ApplicationController
    end
 
    def add_to_cart
-      @product = Product.find(params[:id])
-      current_cart.add_product_to_cart(@product)
-      flash[:notice]="成功加入购物车"
+           @product = Product.find(params[:id])
+     if !current_cart.products.include?(@product)
+         flash[:notice]="你已经将 #{@product.title} 加入购物车"
+         current_cart.add_product_to_cart(@product)
+     else
+         flash[:warning]= "你的购物车内已有此物品"
+     end
       redirect_to :back
 
    end
