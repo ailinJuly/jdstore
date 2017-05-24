02c0fe581e64717be726f726fd29165f70647bc5
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-05-24 22:20:16 +0800

    merge the code 0524

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap/alert
-//= require bootstrap/dropdown
-
 //= require_tree .
+
+$(document).on('click', '.productDecription-menuList-item', function () {
+  var index = $(this).index() //拿到这个「选项卡」的index，第一个是0，第二个是1，以此类推
+  $('.productDecription-content-item').hide() //所有的内容都隐藏
+  $('.productDecription-content-item').eq(index).show() //只显示对于index的内容
+})
