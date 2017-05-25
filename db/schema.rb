b8a63e145d6a09db5425132535012472515ba6a2
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-05-25 19:17:31 +0800

    adjust font

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525091447) do
+ActiveRecord::Schema.define(version: 20170525092133) do
 
   create_table "appointments", force: :cascade do |t|
     t.string   "name"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20170525091447) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.string   "category"
   end
 
   create_table "users", force: :cascade do |t|
