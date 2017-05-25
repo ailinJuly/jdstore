3d07ca5dc819bb37f3b1749e163abb69a7a3cfac
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-05-25 22:29:58 +0800

    implement product list

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,7 +19,7 @@ class ImageUploader < CarrierWave::Uploader::Base
    process resize_to_fit: [800, 800]
 
  version :thumb do
-   process resize_to_fill: [200,200]
+   process resize_to_fill: [356,220]
  end
 
  version :medium do
