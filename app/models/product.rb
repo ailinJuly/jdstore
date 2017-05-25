2dd7a76d441fde2a38aa8570563b526dd0f59bce
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-05-25 10:18:01 +0800

    implement products favorite

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
 
   scope :recent, ->{ order('created_at DESC')  }
   mount_uploader :image, ImageUploader
+  has_many :favorites
+  has_many :member_products, through: :favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   end
 
   has_many :orders
+  has_many :favorites
+  has_many :favorite_products, :through => :favorites, :source => :product
 end
