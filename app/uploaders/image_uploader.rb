12112b559018fa75b11bdc7e7e5ec3d14829f88e
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-05-26 21:04:29 +0800

    add the logo and appointment enter

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
  end
 
  version :medium do
-   process resize_to_fill: [356,356]
+   process resize_to_fill: [420,420]
  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
