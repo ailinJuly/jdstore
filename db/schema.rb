d9593d695b6569cf7777f09c39b6bde0da5d26e3
peterswj <peterswj@163.com>
2017-06-02 21:11:43 +0800

    initial env

--- db/schema.rb
+++ db/schema.rb
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20170601115848) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.string   "category"
     t.integer  "category_id"
   end
 
